2c327acbb644bf22707b5c6ec1769b1e0f84d4e3
CM Lubinski <christopher.lubinski@gsa.gov>
2014-09-11 21:19:48 +0000

    Start of foia contact scraper migration. Addition of travis

---
+++ foia_hub/settings/test.py
@@ -0,0 +1,13 @@
+from foia_hub.settings.default import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '#-nl=1b8yr*zr&6dmnv8rj5(f8w7^lv6lyd)7eyjg_xqk$zhe$'
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3'
+    }
+}
