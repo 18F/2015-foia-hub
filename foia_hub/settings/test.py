4fd9ad85f37e8575cde16e4f50845e4a6e1b3bfd
Eric Mill <eric.mill@gsa.gov>
2014-09-15 20:54:21 -0400

    remove comments

--- foia_hub/settings/test.py
+++ foia_hub/settings/test.py
@@ -2,9 +2,6 @@ from .base import *
 
 SECRET_KEY = '#-nl=1b8yr*zr&6dmnv8rj5(f8w7^lv6lyd)7eyjg_xqk$zhe$'
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3'
