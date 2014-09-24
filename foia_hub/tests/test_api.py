d44cb0966c922ee0cf7f709b88cce0ef4562a459
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2014-09-24 18:04:25 +0000

    add tests for AgencyOffice autocomplete api

--- foia_hub/tests/test_api.py
+++ foia_hub/tests/test_api.py
@@ -1,7 +1,9 @@
-from django.test import TestCase
+import json
+from django.test import TestCase, Client
 from foia_hub.models import Agency, Office
 
 from foia_hub.api import agency_preparer, office_preparer
+
 
 class PreparerTests(TestCase):
     def test_agency_preparer(self):
@@ -25,3 +27,23 @@ class PreparerTests(TestCase):
         op = fields_preparer.prepare(office)
         self.assertEqual('office-name', op['name'])
         self.assertEqual('office-slug', op['slug'])
+
+
+class AgencyOfficeAPITests(TestCase):
+    fixtures = ['agencies_test.json', 'offices_test.json']
+
+    def test_autocomplete(self):
+        c = Client()
+        response = c.get('/api/agencyoffice/autocomplete/')
+        content = response.content
+        content = json.loads(content.decode('utf-8'))
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(4, len(content))
+
+        slugs = [a['slug'] for a in content]
+        slugs.sort()
+        self.assertEqual([
+            'census-bureau',
+            'department-of-commerce',
+            'department-of-homeland-security',
+            'federal-emergency-management-agency'], slugs)
