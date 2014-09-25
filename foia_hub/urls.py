ea66259489f5d32af404637268aea417c304d9fe
CM Lubinski <christopher.lubinski@gsa.gov>
2014-09-25 19:54:50 +0000

    Simple 'profile' for agency/office structure

--- foia_hub/urls.py
+++ foia_hub/urls.py
@@ -4,18 +4,22 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 from foia_hub.views import (
+    contact_landing,
     request_start, request_autocomplete, request_form, request_success)
 from foia_hub.api import AgencyResource, OfficeResource, FOIARequestResource
 from foia_hub.api import AgencyOfficeResource
 
 
 # Front-end
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
     url(
         r'^learn/?$',
         TemplateView.as_view(template_name="learn.html"), name='learn'),
 
+    url(r'^contacts/(?P<slug>[-\w]+)/?$', contact_landing,
+        name='contact_landing'),
     url(r'^request/?$', request_start, name='request'),
     url(r'^request/autocomplete?$', request_autocomplete, name='autocomplete'),
     url(r'^request/(?P<slug>[-\w]+)/$', request_form, name='form'),
@@ -23,7 +27,8 @@ urlpatterns = patterns('',
 )
 
 # APIs
-urlpatterns += patterns('',
+urlpatterns += patterns(
+    '',
     url(r'^api/agency/(?P<slug>[-\w]+)/$', include(OfficeResource.urls())),
     url(r'^api/agency/', include(AgencyResource.urls())),
     url(r'^api/agencyoffice/', include(AgencyOfficeResource.urls())),
@@ -32,13 +37,15 @@ urlpatterns += patterns('',
 
 # Admin
 admin.autodiscover()
-urlpatterns += patterns('',
+urlpatterns += patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
 )
 
 # For development
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += patterns('',
+    urlpatterns += patterns(
+        '',
         url(r'^__debug__/', include(debug_toolbar.urls)),
     )
