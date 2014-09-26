bc70053f640e777cbea89fd6a179d5c38d1acbcd
CM Lubinski <christopher.lubinski@gsa.gov>
2014-09-26 20:35:24 +0000

    Carry top-level status through when importing. Remove unused function

--- foia_hub/scripts/load_agency_contacts.py
+++ foia_hub/scripts/load_agency_contacts.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import csv
 import logging
 import os
 import sys
@@ -11,7 +10,7 @@ django.setup()
 
 from django.utils.text import slugify
 
-from foia_hub.models import *
+from foia_hub.models import Agency, Office
 
 logger = logging.getLogger(__name__)
 
@@ -82,101 +81,16 @@ def process_yamls(folder):
             o.emails = dept_rec.get('emails', None)
             o.notes = dept_rec.get('notes', None)
 
-            #FOIA contact
+            # FOIA contact
             o.contact = dept_rec.get('address', None)
 
-            #FOIA public liaison
+            # FOIA public liaison
             o.public_liaison = dept_rec.get('public_liaison', None)
+
+            o.top_level = dept_rec.get('top_level', False)
 
             o.save()
 
-
-def process_agency_csv(data_path):
-
-    csvdata = csv.DictReader(open(csvfile, 'rt'))
-
-    """
-    {'Agency': 'Administrative Conference of the United States',
-     'City': 'Washington',
-     'Department': 'Administrative Conference of the United States',
-     'Email Address': '',
-     'Fax': '(202) 386-7190',
-     'Name': 'Shawne McGibbon',
-     'Notes': '',
-     'Online Request Form': '',
-     'Room Number': 'Suite 706 South',
-     'State': 'DC',
-     'Street Address': '1120 20th Street, NW',
-     'Telephone': '(202) 480-2088',
-     'Title': 'FOIA Officer',
-     'Website': 'http://www.acus.gov/foia/',
-     'Zip Code': '20036'}
-     """
-
-    for row in csvdata:
-
-        # Agencies
-        name = row['Department']
-        slug = slugify(name)[:50]
-        a, created = Agency.objects.get_or_create(
-            slug=slug, name=name
-        )
-        a.save()
-
-        # Offices
-        office_name = row['Agency']
-        slug = slugify(office_name)[:50]
-        o, created = Office.objects.get_or_create(
-            agency=a, slug=slug, name=office_name
-        )
-
-        o.website = row.get('website', None)
-        o.request_form = row.get('Online Request Form', None)
-        o.fax = row.get('fax', None)
-        o.notes = row.get('notes', None)
-        o.save()
-
-        # Person
-        zip_code = row.get('Zip Code', None)
-        phone = row.get('Telephone', None)
-        email = row.get('Email Address', None)
-        state = row.get('State', None)
-
-        # Only create a person record if these exist
-        if name or title:
-            if zip_code or phone or email or state:
-
-                # To fix the following record and any like it.
-                # "20857(if sending by courier use zc 20814)"
-                if len(zip_code) > 10:
-                    zip_code = zip_code[:5]
-
-                # To fix the following record and any like it.
-                # '(800) 375-5283 (USCIS National Customer Service Unit)'
-                if len(phone) > 50:
-                    phone = phone[:14]
-
-                # Note: Some phone fields have 2 phone numbers.
-                # Not sure if one is more important the other.
-                # TODO: Compare if one these numbers is stored with other
-                # person, so it doesn't need to be stored with this on.
-                # example: (202) 343-1743, (866) 431-0486
-
-                person = Person(
-                    email=email, phone=phone,
-
-                    name=row.get('Name', None),
-                    title=row.get('Title', None),
-
-                    street_address=row.get('Street Address', None),
-                    room_number=row.get('Room Number', None),
-                    city=row.get('City', None),
-
-                    state=state, zip_code=zip_code,
-
-                    office=o,
-                )
-                person.save()
 
 if __name__ == "__main__":
 
