9003d5247793eb38963ccb32cf0da0099eaadfa4
CM Lubinski <christopher.lubinski@gsa.gov>
2014-10-10 15:29:44 +0000

    Add 'examples' and 'counter_examples' fields to agency

---
+++ foia_hub/migrations/0007_auto_20141010_1519.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import jsonfield.fields
+import foia_hub.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('foia_hub', '0006_auto_20141002_1930'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='agency',
+            name='counter_examples',
+            field=jsonfield.fields.JSONField(default=foia_hub.models.empty_list),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='agency',
+            name='examples',
+            field=jsonfield.fields.JSONField(default=foia_hub.models.empty_list),
+            preserve_default=True,
+        ),
+    ]
