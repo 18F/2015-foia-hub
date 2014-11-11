34914394a34ddd7345a591240c5b3b6d96c5d389
Eric Mill <eric.mill@gsa.gov>
2014-11-11 12:42:38 -0500

    fix migration 14 to go down properly

--- foia_hub/migrations/0014_freeze_contact_info_on_request.py
+++ foia_hub/migrations/0014_freeze_contact_info_on_request.py
@@ -4,7 +4,16 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 import jsonfield.fields
 import foia_hub.models
+from foia_hub.models import FOIARequest, Office, Agency
 
+# no need to do anything on the way up
+def nothing(apps, schema_editor):
+    pass
+
+# when winding down, requests made to an agency will be deleted.
+def assign_offices(apps, schema_editor):
+    for request in FOIARequest.objects.filter(office=None):
+        request.delete()
 
 class Migration(migrations.Migration):
 
@@ -26,6 +35,13 @@ class Migration(migrations.Migration):
             name='emails',
             field=jsonfield.fields.JSONField(default=foia_hub.models.empty_list, null=True),
             preserve_default=True,
+        ),
+
+        # needs to be done first on the way down, before column removal above
+        # invalidates the lookup call.
+        migrations.RunPython(
+            nothing,
+            reverse_code=assign_offices
         ),
 
     ]
