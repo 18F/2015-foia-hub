422e975b23061d4cb04213f3e9070ee02a3d01a5
Eric Mill <eric.mill@gsa.gov>
2014-11-11 14:20:52 -0500

    first test against request API

--- foia_hub/tests/test_views.py
+++ foia_hub/tests/test_views.py
@@ -7,36 +7,91 @@ from mock import patch
 
 from foia_hub.models import Agency, FOIARequest, Office, Requester
 from foia_hub.views import get_agency_list
-
+from foia_hub.tests import helpers
 
 class RequestFormTests(SimpleTestCase):
     def setUp(self):
-        self.agency = Agency(name='My Agency', zip_code=20404)
+        self.emails = ["foia1@example.com", "foia2@example.com"]
+
+        self.agency = Agency(name='Agency With Offices', zip_code=20404, emails=self.emails)
         self.agency.save()
+
         self.office = Office(
-            agency=self.agency, name='An Office', zip_code=20404)
+            agency=self.agency, name='Office 1', zip_code=20404, emails=self.emails)
         self.office.save()
+
         self.office2 = Office(
-            agency=self.agency, name='Other Office', zip_code=20404)
+            agency=self.agency, name='Office 2', zip_code=20404, emails=self.emails)
         self.office2.save()
+
+        self.agency2 = Agency(name='Agency Without Offices', zip_code=20009, emails=self.emails)
+
         self.requester = Requester.objects.create(
             first_name='Alice', last_name='Bobson', email='eve@example.com')
         self.request = FOIARequest.objects.create(
             requester=self.requester, office=self.office,
             date_end=date.today(), request_body='All the cheese')
 
+    # destroy them all
     def tearDown(self):
-        self.request.delete()
-        self.requester.delete()
-        self.office2.delete()
-        self.office.delete()
-        self.agency.delete()
+        for model in [FOIARequest, Requester, Office, Agency]:
+            model.objects.all().delete()
+
+    def test_submit_request_to_agency(self):
+        requester_email = "requester@example.com"
+        self.assertEqual(0, len(Requester.objects.filter(email=requester_email)))
+
+        response = self.client.post("/api/request/",
+            content_type='application/json',
+            data=json.dumps({
+                'agency': self.agency.slug,
+
+                'email': requester_email,
+                'first_name': "FOIA",
+                'last_name': "Requester",
+
+                'body': "A new request",
+            })
+        )
+
+        if response.status_code == 500:
+            print(response.content)
+
+        self.assertEqual(201, response.status_code)
+        data = helpers.json_from(response)
+        self.assertTrue(data.get('tracking_id') is not None)
+        self.assertEqual('O', data.get('status'))
+
+        request_id = data['tracking_id']
+        foia_request = FOIARequest.objects.get(pk=request_id)
+        self.assertTrue(foia_request is not None)
+        self.assertEqual(1, len(Requester.objects.filter(email=requester_email)))
+        self.assertEqual(requester_email, foia_request.requester.email)
+
+
+
+    # def test_submit_request_to_office(self):
+    #     pass
+
+    # def test_submit_request_to_agency_with_more_data(self):
+    #     response = self.client.post("/api/request/", {
+    #         'agency': self.agency.slug,
+    #         'body': "A new request",
+
+    #         'documents_start': "November 11, 2014",
+    #         'documents_end': "November 21, 2014",
+
+    #         'email': "requester@example.com",
+    #         'fee_limit': 2,
+    #         'first_name': "FOIA",
+    #         'last_name': "Requester",
+    #     })
 
     def test_request_form_successful(self):
         """The agency name should be present in the request form"""
         response = self.client.get(reverse(
             'form', kwargs={'slug': self.agency.slug}))
-        self.assertContains(response, "My Agency")
+        self.assertContains(response, self.agency.name)
 
     def test_request_form_404(self):
         """Should get a 404 if requesting an agency that doesn't exist"""
@@ -48,8 +103,8 @@ class RequestFormTests(SimpleTestCase):
         """Request should be retrieved and displayed"""
         response = self.client.get(reverse(
             'success', kwargs={'id': self.request.id}))
-        self.assertContains(response, "eve@example.com")
-        self.assertContains(response, "My Agency")
+        self.assertContains(response, self.requester.email)
+        self.assertContains(response, self.agency.name)
 
     def test_request_success_404(self):
         """Should get a 404 if trying to get a success page for a request
@@ -105,10 +160,10 @@ class MainPageTests(TestCase):
 
     def test_get_agency_list(self):
         agencies = get_agency_list()
-        self.assertEqual(agencies, 
+        self.assertEqual(agencies,
             [{
                 'name': 'Department of Commerce',
-                'slug': 'department-of-commerce'}, 
+                'slug': 'department-of-commerce'},
              {
                 'name': 'Department of Homeland Security',
                 'slug': 'department-of-homeland-security'}])
