84b35e984e570ceeaef067b392ad4f1f104d6b02
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2014-11-24 13:50:13 +0000

    Removed unused imports

--- foia_hub/migrations/0016_cleaning_stats.py
+++ foia_hub/migrations/0016_cleaning_stats.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations
 from foia_hub.models import Stats
-import jsonfield.fields
 
 # one-way street
 def clear_empty_stats(apps, schema_editor):
