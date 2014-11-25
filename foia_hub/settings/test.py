17607bfaf2f18758bbd7f68ff2868fbeab7001aa
Eric Mill <eric.mill@gsa.gov>
2014-11-25 02:42:00 -0500

    If SHOW_WEBFORM is False in settings, don't link to our webform.
    This allows us to change, in a per-environment way, whether or not
    our app surfaces links to our custom (not yet done) webform.

--- foia_hub/settings/test.py
+++ foia_hub/settings/test.py
@@ -2,6 +2,8 @@ from .base import *
 
 SECRET_KEY = '#-nl=1b8yr*zr&6dmnv8rj5(f8w7^lv6lyd)7eyjg_xqk$zhe$'
 
+SHOW_WEBFORM = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3'
