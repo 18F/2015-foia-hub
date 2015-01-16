d29f07fcfeb43a2e9a5f9dcc2d7b74949ba289e7
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-01-16 22:11:20 +0000

    Add older tests back in

--- foia_hub/tests/test_loading.py
+++ foia_hub/tests/test_loading.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 
 from foia_hub.models import Agency, Office
-from foia_hub.scripts.load_agency_contacts import load_data
+from foia_hub.scripts.load_agency_contacts import (
+    load_data, add_reading_rooms, add_request_time_statistics)
 
 example_office1 = {
     'address': {
@@ -75,7 +76,7 @@ example_agency = {
 }
 
 
-class LoadingTest(TestCase):
+class LoaderTest(TestCase):
     def test_load_data(self):
         load_data(example_agency)
         a = Agency.objects.get(name='Environmental Protection Agency')
@@ -112,3 +113,63 @@ class LoadingTest(TestCase):
         self.assertEqual(
             'environmental-protection-agency-' +
             '-region-9-states-az-ca-hi-nv-as-gu', o.slug)
+
+
+class LoadingTest(TestCase):
+
+    fixtures = ['agencies_test.json']
+
+    def test_add_reading_rooms(self):
+        reading_room_links = [[
+            'Electronic Reading Room', 'http://agency.gov/err/'],
+            ['Pre-2000 Reading Room', 'http://agency.gov/pre-2000/rooms']]
+        agency = Agency.objects.get(slug='department-of-homeland-security')
+        agency = add_reading_rooms(agency, reading_room_links)
+        agency.save()
+
+        # Retrieve saved
+        dhs = Agency.objects.get(slug='department-of-homeland-security')
+        self.assertEqual(2, len(dhs.reading_room_urls.all()))
+        self.assertEqual(
+            'Electronic Reading Room',
+            dhs.reading_room_urls.all()[0].link_text)
+        self.assertEqual(
+            'http://agency.gov/err/',
+            dhs.reading_room_urls.all()[0].url)
+        self.assertEqual(
+            'Pre-2000 Reading Room',
+            dhs.reading_room_urls.all()[1].link_text)
+        self.assertEqual(
+            'http://agency.gov/pre-2000/rooms',
+            dhs.reading_room_urls.all()[1].url)
+
+    def test_add_stats(self):
+        """
+        Confirms all latest records are loaded, no empty records
+        are created, and records with a value of `less than one`
+        are flagged.
+        """
+        # Load data
+        agency = Agency.objects.get(slug='department-of-homeland-security')
+        data = {'request_time_stats': {
+            '2012': {'simple_median_days': '2'},
+            '2014': {'simple_median_days': 'less than 1'}}}
+        add_request_time_statistics(data, agency)
+
+        # Verify latest data is returned when it exists
+        retrieved = agency.stats_set.filter(
+            stat_type='S').order_by('-year').first()
+        self.assertEqual(retrieved.median, 1)
+
+        # Verify that `less than one` records are flagged
+        retrieved = agency.stats_set.filter(
+            stat_type='S').order_by('-year').first()
+        self.assertEqual(retrieved.less_than_one, True)
+
+        # Verify that no empty records are created
+        retrieved = agency.stats_set.filter(
+            stat_type='C').order_by('-year').first()
+        self.assertEqual(retrieved, None)
+        with self.assertRaises(AttributeError) as error:
+            retrieved.median
+        self.assertEqual(type(error.exception), AttributeError)
