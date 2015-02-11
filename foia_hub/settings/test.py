8c961f32ce285c64cb178c01e763a89f52f1e625
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-02-11 23:10:55 +0000

    Add NAME to the test database; unclear why this worked before

--- foia_hub/settings/test.py
+++ foia_hub/settings/test.py
@@ -8,6 +8,7 @@ ANALYTICS_ID = 'MyAwesomeAnalyticsCode'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
