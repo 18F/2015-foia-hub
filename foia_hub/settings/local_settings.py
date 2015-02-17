63920bd92198a12cfc4ce42a20d50e90f74cdc01
Eric Mill <eric.mill@gsa.gov>
2015-02-16 23:48:11 -0500

    rework settings to load db from a URL

--- foia_hub/settings/local_settings.py
+++ foia_hub/settings/local_settings.py
@@ -1,41 +1,10 @@
 from .base import *
 import os
+import dj_database_url
 
-SECRET_KEY = os.getenv("FOIA_SECRET_SESSION_KEY") or 'SET THIS PLEASE'
+# See env.example for an explanation of these settings.
 
-# If the DB engine is defined, it's assumed the rest are too.
-db = os.getenv("FOIA_DB_ENGINE")
-if db:
-  DATABASES = {
-      'default': {
-          'ENGINE': db,
-      }
-  }
-
-  if os.getenv("FOIA_DB_NAME"):
-    DATABASES['default']['NAME'] = os.getenv("FOIA_DB_NAME")
-  if os.getenv("FOIA_DB_USER"):
-    DATABASES['default']['USER'] = os.getenv("FOIA_DB_USER")
-  if os.getenv("FOIA_DB_PASSWORD"):
-    DATABASES['default']['PASSWORD'] = os.getenv("FOIA_DB_PASSWORD")
-  if os.getenv("FOIA_DB_HOST"):
-    DATABASES['default']['HOST'] = os.getenv("FOIA_DB_HOST")
-  if os.getenv("FOIA_DB_PORT"):
-    DATABASES['default']['PORT'] = os.getenv("FOIA_DB_PORT")
-
-# Defaults to a sqlite3 db in project root called default.db
-else:
-  DATABASES = {
-      'default': {
-          'ENGINE': 'django.db.backends.sqlite3',
-          'NAME': 'default.db'
-      }
-  }
-
-webform = os.getenv("FOIA_SHOW_WEBFORM")
-if webform and (webform.lower() != "false"):
-  SHOW_WEBFORM = True
-else:
-  SHOW_WEBFORM = False
-
-ANALYTICS_ID = os.getenv("FOIA_ANALYTICS_ID") or ""
+SECRET_KEY = os.getenv("FOIA_SECRET_SESSION_KEY")
+DATABASES = {'default': dj_database_url.parse(os.getenv("FOIA_DB"))}
+SHOW_WEBFORM = (os.getenv("FOIA_SHOW_WEBFORM") == "true")
+ANALYTICS_ID = os.getenv("FOIA_ANALYTICS_ID")
