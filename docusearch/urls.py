34a2fe435b81ad9bba96ab91c21b0fa4ed53cf9f
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-02-17 17:53:13 +0000

    Add in search form, and search results display

--- docusearch/urls.py
+++ docusearch/urls.py
@@ -1,8 +1,24 @@
 from django.conf.urls import patterns, include, url
 
-from docusearch.views import details
+from haystack.forms import FacetedSearchForm
+from haystack.query import SearchQuerySet
+from haystack.views import FacetedSearchView, search_view_factory
+
+from .views import details
+from .models import Document
 
 
-urlpatterns = patterns('',
+sqs = SearchQuerySet().models(Document).highlight().facet('')
+
+urlpatterns = patterns('haystack.views',
+    url(
+        r'^search/',
+        search_view_factory(
+            view_class=FacetedSearchView,
+            template='search/search.html',
+            searchqueryset=sqs,
+            form_class=FacetedSearchForm,
+        ),
+        name='doc_search'),
     url(r'^document/(?P<document_id>\d+)/$', details, name="doc_details")
 )
