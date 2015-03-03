4d39b85cd93dd701e5d8d5da6fdb0863e3552315
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-03-03 22:13:22 +0000

    Add  in S3 bucket information

--- foia_hub/settings/production.py
+++ foia_hub/settings/production.py
@@ -10,6 +10,11 @@ ALLOWED_HOSTS = ['foia.18f.us']
 
 # if testing out production settings in development:
 # ALLOWED_HOSTS = ['foia.18f.us', 'localhost']
+AWS_STORAGE_BUCKET_NAME = 'openfoia-static'
+#AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 try:
     from .local_settings import *
