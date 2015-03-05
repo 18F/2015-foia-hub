1c9cd4999307ff249ea2b8861ece8638c1582da6
Eric Mill <eric.mill@gsa.gov>
2015-03-05 17:54:27 -0500

    default to cloning the data repo out locally for loading data

--- foia_hub/management/commands/load_agency_contacts.py
+++ foia_hub/management/commands/load_agency_contacts.py
@@ -1,16 +1,9 @@
 from django.core.management.base import BaseCommand
-from foia_hub.settings.base import BASE_DIR
+from foia_hub.settings.base import BASE_DIR, DEFAULT_DATA_REPO
 from foia_hub.scripts.load_agency_contacts import process_yamls
-
-# TODO: Figure out repo structure to make this generic.
-# Things are moving around, leaving this here for now.
-
-DEFAULT_YAML_FOLDER = 'foia/contacts/data'
-
-
-def _get_yaml_folder():
-    return BASE_DIR.rstrip('/foia_hub').rstrip('foia-') + DEFAULT_YAML_FOLDER
-
+import os
+import shutil
+import subprocess
 
 class Command(BaseCommand):
 
@@ -22,9 +15,38 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **options):
-        try:
+        if len(args) > 0:
             yaml_folder = args[0]
-        except IndexError:
-            yaml_folder = _get_yaml_folder()
+        else:
+            yaml_folder = download_data()
 
+        print("Data directory: %s" % yaml_folder)
+        print("Loading data...")
         process_yamls(yaml_folder)
+
+# Clone a new copy of the git repo, removing the dir if it exists.
+def download_data():
+    print("No directory given, checking out copy of data from version control.")
+    print("Note: this requires git to be installed.")
+
+    path = temp_home()
+    if os.path.exists(path):
+        print("Deleting old data checkout.")
+        shutil.rmtree(path)
+
+    print("Cloning repository at %s" % DEFAULT_DATA_REPO)
+    clone_repo(DEFAULT_DATA_REPO, path)
+
+    return os.path.join(path, "contacts", "data")
+
+def temp_home():
+    return os.path.join(BASE_DIR, "..", "temp-data")
+
+# test if a command exists, don't print output
+def clone_repo(repo, directory):
+  try:
+    subprocess.check_call(["git", "clone", repo, directory], shell=False)
+    return True
+  except subprocess.CalledProcessError as exc:
+    print("Error running: %s" % (str(command)))
+    return False
