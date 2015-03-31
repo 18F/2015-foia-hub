9bd1a86594924ff7a60df48e8d03b599e49be55c
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-03-31 18:18:56 +0000

    Move migrations around, add new migration

--- docusearch/migrations/0005_auto_20150331_1809.py
+++ docusearch/migrations/0005_auto_20150331_1809.py
@@ -7,14 +7,14 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('docusearch', '0005_auto_20150220_1909'),
+        ('docusearch', '0004_auto_20150318_2119'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='ImportLog',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
                 ('agency_slug', models.CharField(max_length=100)),
                 ('office_slug', models.CharField(null=True, max_length=100)),
                 ('directory', models.CharField(max_length=100)),
@@ -23,5 +23,13 @@ class Migration(migrations.Migration):
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='importlog',
+            unique_together=set([('agency_slug', 'office_slug', 'directory')]),
+        ),
+        migrations.AlterIndexTogether(
+            name='importlog',
+            index_together=set([('agency_slug', 'office_slug', 'directory')]),
         ),
     ]
