000f6e51aa7171b64116116a3c7c9f9fede6f9ca
ramirezg <gabriel.ramirez@gsa.gov>
2015-04-28 10:49:05 -0400

    moving settings shared between staging and production to notdev

--- foia_hub/settings/notdev.py
+++ foia_hub/settings/notdev.py
@@ -3,6 +3,12 @@ from .base import *
 # There are common settings between staging and production. This puts them
 # all in one place.
 
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+# Flag to determine whether the FOIA request form gets shown.
+SHOW_WEBFORM = False
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
@@ -13,3 +19,16 @@ CACHES = {
         },
     }
 }
+
+AWS_STORAGE_BUCKET_NAME = os.getenv('FOIA_S3_STATIC_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+# Don't add complex authentication related query parameters for requests
+AWS_QUERYSTRING_AUTH = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
--- foia_hub/settings/staging.py
+++ foia_hub/settings/staging.py
@@ -1,23 +1,4 @@
 from .notdev import *
 
-DEBUG = False
-TEMPLATE_DEBUG = False
-
-# Flag to determine whether the FOIA request form gets shown.
-SHOW_WEBFORM = False
-
 # Some possible hosts
 ALLOWED_HOSTS = ['openfoia-staging.cf.18f.us', '127.0.0.1']
-
-AWS_STORAGE_BUCKET_NAME = os.getenv('FOIA_S3_STATIC_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-# Don't add complex authentication related query parameters for requests
-AWS_QUERYSTRING_AUTH = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
