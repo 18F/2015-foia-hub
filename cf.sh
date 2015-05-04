9147599d30f9d9ece5402ee4644f8e2d1986add2
ramirezg <gabriel.ramirez@gsa.gov>
2015-05-03 22:12:05 -0400

    fixing loading function to account for Gunicorn's multiple instances
    fixing documentation

--- cf.sh
+++ cf.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "------ Starting APP ------"
-gunicorn foia_hub.wsgi:application --log-file -
+if [ $CF_INSTANCE_INDEX = "0" ]; then
+    echo "----- Migrating Database -----"
+    python manage.py migrate --noinput
+    echo "----- Loading Agency Contacts -----"
+    python manage.py load_agency_contacts
+fi
+waitress-serve --port=$VCAP_APP_PORT foia_hub.wsgi:application
