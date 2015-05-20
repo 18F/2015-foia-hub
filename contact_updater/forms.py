f4f8763686dec7a089eec1fa2cda9e977e0b778c
ramirezg <gabriel.ramirez@gsa.gov>
2015-05-20 08:48:30 -0400

    adding class attributes to form fields

--- contact_updater/forms.py
+++ contact_updater/forms.py
@@ -22,55 +22,88 @@ class AgencyForm(forms.Form):
     description = forms.CharField(
         max_length=500,
         required=False,
-        widget=forms.Textarea(attrs={'rows': 3}))
+        widget=forms.Textarea(attrs={'rows': 3, 'class': 'description'}))
 
     # Public Liaison
-    public_liaison_name = forms.CharField(label='Name', required=False)
-    public_liaison_email = forms.EmailField(label='Email', required=False)
+    public_liaison_name = forms.CharField(
+        label='Name',
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'public_liaison_name'}))
+    public_liaison_email = forms.EmailField(
+        label='Email',
+        required=False,
+        widget=forms.EmailInput(attrs={'class': 'public_liaison_email'}))
     public_liaison_phone = forms.RegexField(
         label='Phone Number',
         required=False,
         regex=PHONE_RE,
-        error_message=('Must contain a valid phone number'))
+        error_message=('Must contain a valid phone number'),
+        widget=forms.TextInput(attrs={'class': 'public_liaison_phone'}))
 
     # Request Center
     phone = forms.RegexField(
         label='Phone Number',
         required=False,
         regex=PHONE_RE,
-        error_message=('Must contain a valid phone number'))
+        error_message=('Must contain a valid phone number'),
+        widget=forms.TextInput(attrs={'class': 'phone'}))
 
     # FOIA Request Submission
-    address_line_1 = forms.CharField(label='Mailing address 1', required=False)
-    address_line_2 = forms.CharField(label='Mailing address 2', required=False)
-    city = forms.CharField(required=False)
+    address_line_1 = forms.CharField(
+        label='Mailing address 1',
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'address_line_1'}))
+    address_line_2 = forms.CharField(
+        label='Mailing address 2',
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'address_line_2'}))
+    city = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'city'}))
     state = USStateField(
-        widget=forms.Select(choices=STATE_CHOICES), required=False)
-    zip_code = USZipCodeField(required=False)
-
+        required=False,
+        widget=forms.Select(
+            choices=STATE_CHOICES, attrs={'class': 'state'}))
+    zip_code = USZipCodeField(
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'zip_code'}))
     office_url = forms.URLField(
-        label="Website URL", required=False)
-    emails = forms.EmailField(label='Email', required=False)
+        label="Website URL",
+        required=False,
+        widget=forms.URLInput(attrs={'class': 'office_url'}))
+    emails = forms.EmailField(
+        label='Email',
+        required=False,
+        widget=forms.EmailInput(attrs={'class': 'email'}))
 
     fax = forms.RegexField(
         label='Fax Number',
         required=False,
         regex=PHONE_RE,
-        error_message=('Must contain a valid phone number'))
+        error_message=('Must contain a valid phone number'),
+        widget=forms.TextInput(attrs={'class': 'fax'}))
 
     # Other
     component_url = forms.URLField(
-        label="Agency/component website", required=False)
+        label="Agency/component website",
+        required=False,
+        widget=forms.URLInput(attrs={'class': 'component_url'}))
     foia_libraries = forms.URLField(
-        label="Reading room website", required=False)
+        label="Reading room website",
+        required=False,
+        widget=forms.URLInput(attrs={'class': 'foia_libraries'}))
     regulations_website = forms.URLField(
         label="Regulations Website", required=False)
     common_requests = forms.CharField(
         label='Commonly requested topics',
-        required=False, widget=forms.Textarea(attrs={'rows': 4}))
+        required=False,
+        widget=forms.Textarea(attrs={'rows': 4, 'class': 'common_requests'}))
     no_records_about = forms.CharField(
         label='Commonly misdirected topics',
-        required=False, widget=forms.Textarea(attrs={'rows': 4}))
+        required=False,
+        widget=forms.Textarea(attrs={'rows': 4, 'class': 'no_records_about'}))
     request_instructions = forms.CharField(
         label='Request instructions',
-        required=False, widget=forms.Textarea(attrs={'rows': 4}))
+        required=False,
+        widget=forms.Textarea(
+            attrs={'rows': 4, 'class': 'request_instructions'}))
