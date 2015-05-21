09834522a60efc1b6dfe83bf6985c32433a7d6da
ramirezg <gabriel.ramirez@gsa.gov>
2015-05-21 17:27:11 -0400

    adding front-end req. to forms

--- foia_hub/static/js/contact-updater/form-controls.js
+++ foia_hub/static/js/contact-updater/form-controls.js
@@ -1,3 +1,9 @@
+// Make some fields required
+$(function(){
+   $(".description,.public_liaison_phone,.phone,.address_line_1,.city,.state,.zip_code,.component_url").prop('required', true)
+});
+
+// Make forms visible
 $( "#form_selector" ).change( function() {
     // Keep the rest of the forms hidden
     $("div[id^=office_]").hide();
