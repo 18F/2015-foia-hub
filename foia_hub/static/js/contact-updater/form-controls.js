a6909531d47bbfde0665c8742e6f1ac90899559b
ramirezg <gabriel.ramirez@gsa.gov>
2015-05-22 15:13:08 -0400

    including the validation library and adding char counter to front-end

--- foia_hub/static/js/contact-updater/form-controls.js
+++ foia_hub/static/js/contact-updater/form-controls.js
@@ -3,6 +3,16 @@ $(function(){
    $(".description,.public_liaison_phone,.phone,.address_line_1,.city,.state,.zip_code,.component_url").prop('required', true)
 });
 
+jQuery(function ($) {
+    $('form').validatr();
+});
+
+$('form').validatr('addTest', {
+    phone: function (element) {
+        console.log('here');
+    }
+});
+
 // Make forms visible
 $( "#form_selector" ).change( function() {
     // Keep the rest of the forms hidden
@@ -11,4 +21,26 @@ $( "#form_selector" ).change( function() {
     // Show selected form
     document.getElementById("office_" + form_id).style.display = "block";
     $("#office_" + form_id).show();
+});
+
+
+$(document).ready(function() {
+
+    // Set max
+    var max = 500;
+
+    // Init the char number
+    $('#charNum').text(max - $('.description').val().length + '/500 characters');
+
+    // Set a lister to change on keyup
+    $('.description').keyup(function () {
+
+        var len = $(this).val().length;
+        if (len >= max) {
+        $('#charNum').text('You have reached the limit');
+        } else {
+            var char = max - len;
+            $('#charNum').text(char + '/500 characters');
+        }
+    });
 });
