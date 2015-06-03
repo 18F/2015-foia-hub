33210c0bfae54d81caae242522ada3792bcafb88
ramirezg <gabriel.ramirez@gsa.gov>
2015-06-03 16:55:33 -0400

    removing location styling from validatr

--- foia_hub/static/js/contact-updater/form-controls.js
+++ foia_hub/static/js/contact-updater/form-controls.js
@@ -1,28 +1,24 @@
 // Make some fields required
 $(function(){
-   $(".description,.public_liaison_phone,.phone,.address_line_1,.city,.state,.zip_code,.component_url").prop('required', true)
+   $(".agency_description,.public_liaison_phone,.phone,.address_line_1,.city,.state,.zip_code,.component_url").prop('required', true)
 });
 
+// Set up Validatr
 jQuery(function ($) {
-    $('form').validatr();
-});
-
-$('form').validatr('addTest', {
-    phone: function (element) {
-        console.log('here');
-    }
+    $('form').validatr({'showall': true});
 });
 
 // Make forms visible
-$( "#form_selector" ).change( function() {
-    // Keep the rest of the forms hidden
-    $("div[id^=office_]").hide();
-    var form_id = document.getElementById("form_selector").value;
-    // Show selected form
-    document.getElementById("office_" + form_id).style.display = "block";
-    $("#office_" + form_id).show();
-});
-
+$(function(){
+    $( "#form_selector" ).change( function() {
+        // Keep the rest of the forms hidden
+        $("div[id^=office_]").hide();
+        var form_id = document.getElementById("form_selector").value;
+        // Show selected form
+        document.getElementById("office_" + form_id).style.display = "block";
+        $("#office_" + form_id).show();
+    });
+})
 
 $(document).ready(function() {
 
@@ -30,10 +26,10 @@ $(document).ready(function() {
     var max = 500;
 
     // Init the char number
-    $('#charNum').text(max - $('.description').val().length + '/500 characters');
+    $('#charNum').text(max - $('.agency_description').val().length + '/500 characters');
 
     // Set a lister to change on keyup
-    $('.description').keyup(function () {
+    $('.agency_description').keyup(function () {
 
         var len = $(this).val().length;
         if (len >= max) {
