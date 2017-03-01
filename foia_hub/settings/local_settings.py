91f45ecb6ad7fdee84ffbe4eee76356a3df17cbc
James C. Scott <jamesc.scott@gsa.gov>
2017-03-01 12:22:06 -0500

    remove unused env vars

--- foia_hub/settings/local_settings.py
+++ foia_hub/settings/local_settings.py
@@ -17,6 +17,3 @@ SECRET_KEY = os.getenv("FOIA_SECRET_SESSION_KEY")
 DATABASES = {'default': dj_database_url.parse(database_url)}
 SHOW_WEBFORM = (os.getenv("FOIA_SHOW_WEBFORM") == "true")
 ANALYTICS_ID = os.getenv("FOIA_ANALYTICS_ID")
-
-AWS_ACCESS_KEY_ID = os.getenv('FOIA_AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('FOIA_AWS_SECRET_ACCESS_KEY')
